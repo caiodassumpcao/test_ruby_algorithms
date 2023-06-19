1bc2b9e7cc09cb495b7e8e59deb2c165d1d7bf97
caiodassumpcao <caio.rodri.d@gmail.com>
2023-06-19 06:35:43 -0300

    fix technical_test_1_q4.rb: Print input new database

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem "rails", "~> 7.0.4", ">= 7.0.4.3"
 gem "sprockets-rails"
 
 # Use sqlite3 as the database for Active Record
-gem "sqlite3", "~> 1.4"
+# gem "sqlite3", "~> 1.4"
 
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", "~> 5.0"
@@ -45,7 +45,7 @@ gem "bootsnap", require: false
 # Use Sass to process CSS
 # gem "sassc-rails"
 gem "activerecord"
-gem "mysql2"
+gem "mysql2", "~> 0.5.3"
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- lib/technical_test_1_q4.rb
+++ lib/technical_test_1_q4.rb
@@ -7,7 +7,7 @@ ActiveRecord::Base.establish_connection(
   host: 'localhost',
   username: 'root',
   password: 'caitole123',
-  database: 'ruby_teste_1_q5_db'
+  database: 'ruby_teste_1_q412_db'
 )
 
 # Definindo uma classe Employee que herda da classe ActiveRecord::Base.
@@ -15,8 +15,6 @@ ActiveRecord::Base.establish_connection(
 # Classe terá acesso as operações de CRUD e os relacionamentos definidos posteriormente.
 class Employee < ActiveRecord::Base
   belongs_to :team # Definindo relacionamentos para a classe "team"
-  # 'belongs_to': Método para estabelecer associação onde um objeto pertence a outro.
-  # Ele é usado no modelo que possui a chave estrangeira para referenciar o modelo pai
   belongs_to :job_title # Definindo relacionamentos para a classe "job_title"
 
   # Método para retornar o nome completo do funcionário
@@ -37,47 +35,52 @@ end
 
 # Criação das tabelas usando migrações (Definindo o esquema do banco de dados usando ActiveRecord)
 ActiveRecord::Schema.define do
-  create_table :employees unless table_exists?(:employees) do |t|
+  create_table :teams, unless_exists: true do |t|
+    t.string :name
+  end unless table_exists?(:teams)
+
+  create_table :job_titles, unless_exists: true do |t|
+    t.string :name
+  end unless table_exists?(:job_titles)
+
+  create_table :employees, unless_exists: true do |t|
     t.string :first_name
     t.string :last_name
     t.string :cpf
     t.integer :team_id
     t.integer :job_title_id
-  end
-
-  create_table :teams unless table_exists?(:teams) do |t|
-    t.string :name
-  end
-
-  create_table :job_titles unless table_exists?(:job_titles) do |t|
-    t.string :name
-  end
+  end unless table_exists?(:employees)
 end
 
-# Inserção dos dados nas tabelas (Cria um novo registro na tabela "teams" com nome "TI", utilizando o método 'create' fornecido pelo ActiveRecord)
-Team.create(name: 'TI')
+# Inserção dos dados nas tabelas
+Team.create(name: 'TI') unless Team.exists?(name: 'TI') # Verifica se a equipe já existe antes de criar
+Team.create(name: 'Suporte') unless Team.exists?(name: 'Suporte') # Verifica se a equipe já existe antes de criar
 
-# Cria novo registro na tabela 'Job_titles'
-JobTitle.create(name: 'Desenvolvedor back-end')
-JobTitle.create(name: 'Suporte')
-JobTitle.create(name: 'Desenvolvedor front-end')
-JobTitle.create(name: 'Encantador de clientes')
+JobTitle.create(name: 'Desenvolvedor back-end') unless JobTitle.exists?(name: 'Desenvolvedor back-end') # Verifica se o cargo já existe antes de criar
+JobTitle.create(name: 'Suporte') unless JobTitle.exists?(name: 'Suporte') # Verifica se o cargo já existe antes de criar
+JobTitle.create(name: 'Desenvolvedor front-end') unless JobTitle.exists?(name: 'Desenvolvedor front-end') # Verifica se o cargo já existe antes de criar
+JobTitle.create(name: 'Encantador de clientes') unless JobTitle.exists?(name: 'Encantador de clientes') # Verifica se o cargo já existe antes de criar
 
-# Cria novo registro na tabela 'team_id e 'job_title_id'
-Employee.create(first_name: 'Pedro', last_name: 'Alves', cpf: '665.415.876-80', team_id: 2, job_title_id: 3)
-Employee.create(first_name: 'Tiago', last_name: 'Nogueira', cpf: '032.336.130-75', team_id: 1, job_title_id: 1)
-Employee.create(first_name: 'João', last_name: 'Neves', cpf: '461.636.517-23', team_id: 1, job_title_id: 2)
+# Cria novo registro na tabela 'team_id' e 'job_title_id' se o colaborador ainda não existe
+Employee.create_with(team_id: Team.find_by(name: 'TI').id, job_title_id: JobTitle.find_by(name: 'Desenvolvedor back-end').id)
+        .find_or_create_by(first_name: 'Tiago', last_name: 'Nogueira', cpf: '032.336.130-75')
 
+Employee.create_with(team_id: Team.find_by(name: 'TI').id, job_title_id: JobTitle.find_by(name: 'Desenvolvedor front-end').id)
+        .find_or_create_by(first_name: 'João', last_name: 'Neves', cpf: '461.636.517-23')
 
-# Impressão das informações dos funcionários
-# Consulta o banco de dados buscando registros da tabela "employees", buscando "teams" e "job_title" ordenados por CPF
-employees = Employee.joins(:team, :job_title).order(:cpf)
+Employee.create_with(team_id: Team.find_by(name: 'Suporte').id, job_title_id: JobTitle.find_by(name: 'Encantador de clientes').id)
+        .find_or_create_by(first_name: 'Pedro', last_name: 'Alves', cpf: '665.415.876-80')
 
-# Print da busca feita anteriormente
+# Recupera todos os colaboradores e exibe suas informações
+employees = Employee.all
+
 employees.each do |employee|
+  puts '------------------------'
   puts "Nome: #{employee.full_name}"
   puts "CPF: #{employee.cpf}"
   puts "Equipe: #{employee.team.name}"
   puts "Cargo: #{employee.job_title.name}"
-  puts "------------------------"
 end
+
+puts '------------------------'
+puts "Os dados estão corretos. Total de colaboradores: #{employees.count}"
