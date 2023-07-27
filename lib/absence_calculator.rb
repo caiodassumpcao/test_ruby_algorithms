7c46eaffcff040ab955bddc842edba32b0889d72
caiodassumpcao <caio.rodri.d@gmail.com>
2023-07-27 13:23:00 -0300

    Feat absence_calculator.rb: Implement SQL database functionality into the program

--- lib/absence_calculator.rb
+++ lib/absence_calculator.rb
@@ -1,4 +1,47 @@
 # frozen_string_literal: true
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+  adapter: 'mysql2',
+  host: 'localhost',
+  username: 'root',
+  password: 'caitole123',
+  database: 'ruby_teste_2_q34_db'
+)
+
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.integer :carga_horaria
+      t.integer :hora_aula
+
+      t.timestamps
+    end
+  end
+end
+
+CreateUsers.new.change
+
+class ApplicationRecord < ActiveRecord::Base
+  self.abstract_class = true
+end
+
+class User < ApplicationRecord
+  scope :materia, -> {
+    where("carga_horaria >= 20 AND hora_aula >= 140")
+  }
+end
+
+User.create(name: "Maquinas Primarias", carga_horaria: 60, hora_aula: 280)
+
+materias = User.materia
+
+materias.each do |cadeira|
+  puts cadeira.name
+  puts cadeira.carga_horaria
+  puts cadeira.hora_aula
+end
 
 puts "Qual a carga horaria da cadeira em horas?"
 carga_horaria = gets.chomp.to_i * 60
