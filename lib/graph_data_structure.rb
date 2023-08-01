cfe1c12d4d20c40c4ad37672089ebe85117bd2d0
caiodassumpcao <caio.rodri.d@gmail.com>
2023-08-01 12:17:09 -0300

    Fix graph_data_structure.rb: Max Flow Problem - Fill the Tank

--- lib/graph_data_structure.rb
+++ lib/graph_data_structure.rb
@@ -19,6 +19,27 @@ require ''
 require ''
 
 def max_flow_fill_tank(n, s, edges, capacities)
+  # Criamos um grafo direcionado para representar a conexão entre os tanques.
   g = RGL::DirectedAdjacencyGraph.new
-  capacities.each
+
+  # Iterando sobre o array capacities, que contém as capacidades dos tanques. Para cada capacidade c presente no array, adicionamos um vértice ao grafo g. Em outras palavras, cada capacidade se torna um vértice no grafo, representando um tanque
+  capacities.each { |c| g.add_vertex(c) }
+
+  #Iterando sobre o array "edges" que contem as arestas do grafo. Array com objetos de dois elementos (Primeiro elemento: Origen "u",Segundo elemento: Destino "v") representando a conexao entre os vertices (tanque)
+  edges.each { |u, v| g.add_edge(u, v) }
+
+  # Criando Hash para armazenar o fluxo entre os tanques
+  flow_hash = {}
+
+  # Iterando sobre array capacities, para cada capacidade 'v' estamos criando um novo hash vazio associado a essa capacidade
+  # Para cada tanque (representado pela sua capacidade) criamos um hash vazio que sera usado para armazenar os fluxos entre os tanques
+  capacities.each { |v| flow_hash[v] = Hash.new(0) }
+
+  #
+  total_flow = 0
+  g.flow do |u, v, flow|
+    flow_hash[u][v] = flow
+    total_flow += flow if u == s
+  end
+
 end
