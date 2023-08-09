0a7ef9199f3cf7934e926a62d1c71414e799a9cd
caiodassumpcao <caio.rodri.d@gmail.com>
2023-08-09 08:55:15 -0300

    Fix library_system.rb and training_class.rb: Practicing classes

--- lib/training_class.rb
+++ lib/training_class.rb
@@ -1,7 +1,28 @@
 
-class Tft
-  def jogo
-    puts "Teamfight Tactics"
+class Personagem
+  def custo_5
+    puts "Criado personagem custo 5"
   end
 end
 
+heimer = Personagem.new
+heimer.custo_5
+sion = Personagem.new
+"Sion".send(:length)
+sion.send(:custo_5)
+
+class Teste
+  self.class_eval {
+    define_method(:'hey there') { puts 'Hey there!'}
+  }
+end
+foo = Teste.new
+foo.send :'hey there'
+
+class Integer
+  def more
+    return self + 1
+  end
+end
+puts 3.more
+puts 7.more
