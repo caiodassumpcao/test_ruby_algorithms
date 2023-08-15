79c30394b06a503e3a509f1879f4f509820757a3
caiodassumpcao <caio.rodri.d@gmail.com>
2023-08-14 22:51:03 -0300

    Fix teste  technical_test_1_q2_spec.rb.rb: Create test Rspec

--- spec/technical_test_1_q2_spec.rb
+++ spec/technical_test_1_q2_spec.rb
@@ -3,12 +3,24 @@ require 'technical_test_1_q2'
 
 RSpec.describe '#Validar_CPF' do
   describe '#valida_cpf_formatado' do
-    context 'when the cpf has less 11 number ' do
       it 'return true' do
         valid_cpf = '123.456.789-00'
+        expect(valida_cpf_formatado(valid_cpf)).to be true
+
+      end
+      it 'returne false for invalid formatted CPF' do
+        invalid_cpf = '123.456.789-AB'
+        expect(valida_cpf_formatado(invalid_cpf)).to be false
       end
 
-    end
+      it 'returns false for CPF whithout formatting' do
+        invalid_cpf = '12345678900'
+        expect(valida_cpf_formatado(invalid_cpf)).to be false
+      end
 
+      it ' returns false for valid format with incorrect digits' do
+        valid_cpf = '123.456.789-10'
+        assert(valida_cpf_formatado(valid_cpf) == false)
+      end
   end
 end
