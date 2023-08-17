59c1e960defd18aa74d0dc05ced5b2d6b73acc38
caiodassumpcao <caio.rodri.d@gmail.com>
2023-08-16 22:25:15 -0300

    Fix teste  technical_test_1_q5_spec.rb.rb: Create test Rspec

--- spec/technical_test_1_q5_spec.rb
+++ spec/technical_test_1_q5_spec.rb
@@ -1,6 +1,21 @@
 require 'rspec-rails'
 require 'technical_test_1_q5'
+require 'active_record'
 
 RSpec.describe 'Biblioteca' do
+  before(:all) do
+    ActiveRecord::Base.establish_connection(
+      adapter: 'sqlite3',
+      database: ':memory'
+    )
+    ActiveRecord::Schema.define do
+      create_table :teams, unless_exists: true do |t|
+        t.string :name
+      end unless  table_exists?(:teams)
 
+      create_table :employees, unless_exists: true do |t|
+
+      end
+    end
+  end
 end
