ca341001377c295410a0accb3a1ceb4c10833db4
caiodassumpcao <caio.rodri.d@gmail.com>
2023-08-16 23:10:02 -0300

    Fix teste  technical_test_1_q5_spec.rb.rb: Create test Rspec

--- spec/technical_test_1_q5_spec.rb
+++ spec/technical_test_1_q5_spec.rb
@@ -25,6 +25,44 @@ RSpec.describe 'Biblioteca' do
     Team.create(name: 'TI') unless Team.exists?(name: 'TI')
     Team.create(name: 'Suporte') unless Team.exists?(name: 'Suporte')
 
-    JobTitle.create(name: 'Desenvolvedor back-end') unless JobTitle.exists?(method_name)
+    JobTitle.create(name: 'Desenvolvedor back-end') unless JobTitle.exists?(name: 'Desenvolvedor back-end')
+    JobTitle.create(name: 'Suporte') unless JobTitle.exists?(name: 'Suporte')
+    JobTitle.create(name: 'Desenvolvedor front-end') unless JobTitle.exists?(name: 'Desenvolvedor front-end')
+  end
+
+  after(:all) do
+    Employee.delete_all
+    Team.delete_all
+    JobTitle.delete_all
+  end
+
+  describe 'Funcionarios' do
+    context 'Operaçao basica' do
+      it 'Criar, ler, atualizar e deletar funcionarios' do
+        employee = Employee.create(first_name: 'Caio', last_name: 'Rodrigues', cpf: '123.456.789-00')
+        read_employee = Employee.find(employee.id)
+        expect(read_employee.first_name).to eq('Caio')
+
+        employee.destroy
+        expect(Employee.exists?(employee.id)).to be_falsey
+      end
+    end
+
+    context 'Relacionamento com equipe e cargo' do
+      it 'return a complet name' do
+        team = Team.find_by(name: 'TI')
+        job_title = JobTitle.find_by(name: 'Desenvolvedor back-end')
+        employee = Employee.create(first_name: 'Jack', last_name: 'Dassumpcao', cpf: '123.456.789-00', team: team, job_title: job_title)
+        expect(employee.team).to eq(team)
+        expect(employee.job_title).to eq(job_title)
+      end
+    end
+
+    context 'Metodo full_name' do
+      it 'retorna o nome completo do funcionario' do
+        employee = Employee.create(:first_name: 'Malu', last_name: 'Rodrigues', cpf: '987.654.321-00')
+        expect(employee.full_name).to eq('Malu Rodrigues')
+      end
+    end
   end
 end
