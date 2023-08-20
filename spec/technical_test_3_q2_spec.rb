53dd54548f3eb524fa1f8e076f0d43e646d8d7a0
caiodassumpcao <caio.rodri.d@gmail.com>
2023-08-20 12:52:00 -0300

    Fix test technical_test_3_q2_spec.rb.rb: Create test Rspec

--- spec/technical_test_3_q2_spec.rb
+++ spec/technical_test_3_q2_spec.rb
@@ -10,9 +10,14 @@ RSpec.describe 'solucao' do
     ]
 
     format_config = {
-      name: { length: 15,}
+      name: { length: 15, padding: 'zeroes', aling: 'left' },
+      cpf: { length: 14, padding: 'spaces', aling: 'rogth' },
+      state: { length: 20, padding: 'spaces', aling: 'left' },
+      value: { length: 10, padding: 'zeroes', aling: 'right'}
     }
 
+    expected_output = "Nome:000Maria Neusa, CPF:97905796671, Estado:Sao Paulo        , Valor:001234\n" +
+                  "Nome:000Ricardo Font, CPF:44010762900,  "
   end
 
 end
