ccf433dce2eb6fece181c9251863bbb355e3ef05
caiodassumpcao <caio.rodri.d@gmail.com>
2023-08-28 11:53:48 -0300

    Fix fill_tank_spec.rb: Test Rspec

--- lib/fill_tank.rb
+++ lib/fill_tank.rb
@@ -13,6 +13,8 @@ def max_flow_fill_tank(n, s, edges, capacities)
   capacities.each { |v| flow_hash[v] = Hash.new(0) }
 
   total_flow = 0
+
+  #Calcular o fluxo maximo usando o algoritmo de fluxo de redes
   g.flow do |u, v, flow|
     flow_hash[u][v] = flow
     total_flow += flow if u == s
@@ -20,10 +22,11 @@ def max_flow_fill_tank(n, s, edges, capacities)
   total_flow
 end
 
-
+#Definir os valores do problema
 N = 4
 S = 1
 Edges = [[1, 2], [1, 3], [2, 4]]
 Cap = [1, 1, 1, 1]
 
+#Calcular o fluxo maximo usando a funçao e imprimir o resultado
 puts max_flow_fill_tank(N, S, Edges, Cap)
